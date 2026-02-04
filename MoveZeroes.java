698a9c087a1f0fc1a01f2f2856d8de78841df0a4
vtu24382-rgb <vtu24382@veltech.edu.in>
2026-02-04 19:24:08 +0530

    Remove all files

--- MoveZeroes.java
+++
@@ -1,9 +0,0 @@
-class MoveZeroes {
-    public void moveZeroes(int[] nums) {
-        int idx = 0;
-        for (int num : nums) {
-            if (num != 0) nums[idx++] = num;
-        }
-        while (idx < nums.length) nums[idx++] = 0;
-    }
-}
