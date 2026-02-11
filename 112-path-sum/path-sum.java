1aab00155e84f29bdb17fa09f176e90fb65701cc
vtu24382-rgb <vtu24382@veltech.edu.in>
2026-02-11 11:17:22 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (13.44%) - LeetSync

---
+++ 112-path-sum/path-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null)
+            return false;
+        
+        // If it's a leaf node
+        if (root.left == null && root.right == null)
+            return targetSum == root.val;
+        
+        // Check left or right subtree
+        int remainingSum = targetSum - root.val;
+        
+        return hasPathSum(root.left, remainingSum) ||
+               hasPathSum(root.right, remainingSum);
+    }
+}
