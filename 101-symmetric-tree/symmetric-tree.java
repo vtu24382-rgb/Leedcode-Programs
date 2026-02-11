b10d313a4e1670563589cd17f12fbf3d9425c8c7
vtu24382-rgb <vtu24382@veltech.edu.in>
2026-02-11 11:04:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (84.02%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root.left, root.right);
+    }
+    
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null)
+            return true;
+        
+        if (left == null || right == null)
+            return false;
+        
+        if (left.val != right.val)
+            return false;
+        
+        return isMirror(left.left, right.right) &&
+               isMirror(left.right, right.left);
+    }
+}
