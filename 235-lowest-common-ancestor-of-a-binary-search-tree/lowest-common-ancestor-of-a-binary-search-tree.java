0449e5c59ef7aab9787e57952ddf24b853cf8a61
vtu24382-rgb <vtu24382@veltech.edu.in>
2026-04-08 09:48:33 +0530

    Time: 6 ms (97.02%) | Memory: 47.8 MB (24.61%) - LeetSync

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -0,0 +1,22 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        while (root != null) {
+            
+            // Both nodes in left subtree
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            }
+            // Both nodes in right subtree
+            else if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            }
+            // Split point → LCA found
+            else {
+                return root;
+            }
+        }
+        
+        return null;
+    }
+}
