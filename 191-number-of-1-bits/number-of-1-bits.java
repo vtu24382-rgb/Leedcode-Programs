c468aa10d3eaa3c989909e89837b98137b7048a0
vtu24382-rgb <vtu24382@veltech.edu.in>
2026-07-09 23:12:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (65.53%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int hammingWeight(int n) {
+
+        int result=0;
+
+        while(n!=0){
+            result=result+(n&1);
+            n>>>=1;
+        }
+
+        return result;
+        
+    }
+}
